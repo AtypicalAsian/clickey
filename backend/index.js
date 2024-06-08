90f5779984af7f3e45f1d23707a5094267c97e52
Nicolas Nguyen <nicnguyen2702@gmail.com>
2024-06-08 02:06:07 -0400

    initialize project

---
+++ backend/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+
+//database
+const connectDB = async () => {
+  try {
+  } catch (error) {}
+};
+
+app.listen(5000, () => {
+  console.log("Listening on Port 5000");
+});
